278675c491b9fd92e751ad62c5766cf3b2e31f55
Michael Lennartsson <michael.lennartsson@cybercom.com>
2016-11-09 17:18:47 +0100

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    entry: "./app/index.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loader: "style!css"
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader" }
+        ]
+    }
+};
